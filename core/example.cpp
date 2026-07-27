dfefef0904a5b87b0cd7511db1da4468f7ddc071
JYY <stupid_coder_jyy@163.com>
2026-07-27 20:39:25 +0800

    Initial Commit

---
+++ core/example.cpp
@@ -0,0 +1,5 @@
+//
+// Created by PC on 2026/7/27.
+//
+
+#include "example.h"
---
+++ core/example.h
@@ -0,0 +1,10 @@
+//
+// Created by PC on 2026/7/27.
+//
+
+#ifndef NETFERRY_EXAMPLE_H
+#define NETFERRY_EXAMPLE_H
+
+class example {};
+
+#endif  //NETFERRY_EXAMPLE_H
